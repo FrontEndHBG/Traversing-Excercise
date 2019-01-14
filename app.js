329d9587574c6f7c344bca51dcfed9b06fda9e72
Max Carlquist <tenkaklet@gmail.com>
2019-01-14 18:01:50 +0100

    update to es6

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-$(function () {
-    $('.top h1').text('Traversing is fun!');
-    $('#list').children().text('hey');
-    $('.element').children().addClass('hidden');
+$(() => {
+  
+  $('.top h1').text('Traversing is fun!');
+  $('#list').children().text('hey');
+  $('.element').children().addClass('hidden');
 
-
-  $('.change-show').click(function () {
-      $('.show').toggleClass('hidden');
+  $('.change-show').click(() => {
+    $('.show').toggleClass('hidden');
   });
 
-  $('.add-item').click(function () {
-      $('#list').append('<li>New Item!</li>');
-  });  
+  $('..add-item').click(() => {
+    $('#list').append('<li>New Item!</li>');
+  });
+
 });
